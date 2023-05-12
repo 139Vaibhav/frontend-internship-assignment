918f4ef42edf644e2a24dbfc8fe2d626366ca268
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2023-05-13 00:55:40 +0530

    completed

---
+++ src/app/components/search-feature/search-feature.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'front-end-internship-assignment-search-feature',
+  templateUrl: './search-feature.component.html',
+  styleUrls: ['./search-feature.component.scss'],
+})
+export class SearchFeatureComponent {}
